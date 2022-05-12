da71d90e1e96de21c3da3df9a2c359aa721f87cb
Nadejda <nziruhina@gmail.com>
2022-05-12 20:22:47 +0300

    Work Done

---
+++ Seminar4/Program.cs
@@ -0,0 +1,94 @@
+﻿/*
+
+// Задача 1. Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int[] CreateRandomArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+
+    return array;
+}
+
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+
+    Console.WriteLine();
+}
+
+void EvenElements(int[] array)
+{
+    int count = 0;
+        
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+            count++;
+    }
+
+    Console.WriteLine("Number of even elements in the array: " + count);
+}
+
+
+int[] myArray = CreateRandomArray(12, 100, 999);
+
+ShowArray(myArray);
+
+EvenElements(myArray);
+
+
+// Задача 2. Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+int[] CreateRandomArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+
+    return array;
+}
+
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+
+    Console.WriteLine();
+}
+
+void SumOfOddPositions(int[] array)
+{
+    int sum = 0;
+        
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 == 0)
+        {
+            sum += array[i];
+        }
+    }
+
+    Console.WriteLine("The sum of elements in odd positions: " + sum);
+}
+
+
+int[] myArray = CreateRandomArray(12, -50, 50);
+
+ShowArray(myArray);
+
+SumOfOddPositions(myArray);
+
+*/
