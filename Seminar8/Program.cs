6257773d0910a929d7b97d330beb7e84e28d3a18
Nadejda <nziruhina@gmail.com>
2022-06-06 23:54:39 +0300

    Work Done

---
+++ Seminar8/Program.cs
@@ -0,0 +1,50 @@
+﻿/*
+
+// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по возрастанию элементы каждой строки двумерного массива.
+
+int[,] RandomMatrix(int[,] doublearray)
+{    
+    for (int i = 0; i < doublearray.GetLength(0); i++)
+    {
+        for (int j = 0; j < doublearray.GetLength(1); j++)
+        {
+            doublearray[i,j] = new Random().Next(1,9);
+            Console.Write(doublearray[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+
+    return doublearray;
+}
+
+int[,] SortMatrix(int[,] doublearray)
+{
+    int temp = 0;
+
+    for (int i = 0; i < doublearray.GetLength(0); i++)
+    {
+        for (int j = 0; j < doublearray.GetLength(1); j++)
+        {
+            for (int k = j + 1; k < doublearray.GetLength(1); k++)
+            {
+                if (doublearray[i,j] > doublearray[i,k])
+                {
+                    temp = doublearray[i,k];
+                    doublearray[i,k] = doublearray[i,j];
+                    doublearray[i,j] = temp;
+                }
+            }
+            Console.Write(doublearray[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+
+    return doublearray;
+}
+
+int[,] matrix = new int[3,4];
+
+RandomMatrix(matrix);
+SortMatrix(matrix);
+
+*/
