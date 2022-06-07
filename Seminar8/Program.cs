6c887b954ab435213e9585d8a50d09b335b31640
Nadejda <nziruhina@gmail.com>
2022-06-07 12:45:59 +0300

    Work Done

--- Seminar8/Program.cs
+++ Seminar8/Program.cs
@@ -50,3 +50,58 @@ RandomMatrix(matrix);
 SortMatrix(matrix);
 
 */
+// Задача 2: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+
+void SmallestSumOfElements(int[,] summatrix)
+{
+    int summin = 0;
+    int sum1 = 0;
+    int sum2 = 0;
+    int index = 0;
+
+    int temp = 0;
+    int itemp = 0;
+
+    for (int i = 0; i < summatrix.GetLength(0); i++)
+    {
+        sum2 = 0;
+        
+        for (int j = 0; j < summatrix.GetLength(1); j++)
+        {            
+            sum2 += summatrix[i,j];
+        }
+
+        if (i == 0)
+        {
+            temp = sum2;
+            itemp = i;
+        }
+
+        if (sum2 < sum1)
+        {
+            summin = sum2;
+            index = i;
+        }
+        
+        sum1 = sum2;
+    }
+
+    if (temp < summin)
+    {
+        summin = temp;
+        index = itemp;
+    }
+
+    Console.WriteLine("Line No: " + (index+1));
+    Console.WriteLine("Sum of elements: " + summin);
+    for (int i = 0; i < summatrix.GetLength(1); i++)
+        Console.Write(summatrix[index,i] + " ");
+}
+
+int[,] matrix = {{1,4,7,2},
+                 {5,9,2,3},
+                 {8,4,2,4},
+                 {5,2,6,7}};
+
+SmallestSumOfElements(matrix);
