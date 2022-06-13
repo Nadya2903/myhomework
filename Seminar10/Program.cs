a5c7754d6799fa735117d5cf4f78a2f02a0cb4aa
Nadejda <nziruhina@gmail.com>
2022-06-13 16:15:56 +0300

    Work Done

--- Seminar10/Program.cs
+++ Seminar10/Program.cs
@@ -21,6 +21,39 @@ int Vowel(string[] words)
 
 string[] words = {"qwe", "wer", "ert", "rty", "tyu"};
 
-Console.WriteLine(Vowel(words));
+Console.WriteLine("Number of words starting with a vowel: " + Vowel(words));
+
+
+// Задача 2: Задайте массив строк. Напишите программу, которая генерирует новый массив, объединяя элементы исходного массива попарно.
+
+void Merger(string[] words)
+{
+    for (int i = 0; i < words.Length; i++)
+    {
+        string pair = "";
+        
+        if (i % 2 > 0)
+        {
+            pair = words[i-1] + words[i];
+            Console.Write(pair + " ");
+        }
+    }
+}
+
+void PrintArray(string[] arrtxt)
+{
+    for (int i = 0; i < arrtxt.Length; i++)
+    {
+        Console.Write(arrtxt[i] + " ");
+    }
+
+    Console.WriteLine("\n");
+}
+
+string[] words = {"qwe", "wer", "ert", "rty", "tyu", "yui"};
+
+PrintArray(words);
+
+Merger(words);
 
 */
