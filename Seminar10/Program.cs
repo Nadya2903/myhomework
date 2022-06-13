8ece711ed0971e9480340e3aebb96bde31744574
Nadejda <nziruhina@gmail.com>
2022-06-13 14:08:50 +0300

    Work Done

---
+++ Seminar10/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+
+// Задача 1: Задайте массив строк. Напишите программу, считает кол-во слов в массиве, начинающихся на гласную букву.
+
+int Vowel(string[] words)
+{
+    char[] vowels = {'a', 'e', 'i', 'o', 'u', 'y'};
+    int count = 0;
+
+    for (int i = 0; i < words.Length; i++)
+    {
+        for (int j = 0; j < vowels.Length; j++)
+        {
+            if (words[i][0] == vowels[j])
+                count++;
+        }
+    }
+
+    return count;
+}
+
+string[] words = {"qwe", "wer", "ert", "rty", "tyu"};
+
+Console.WriteLine(Vowel(words));
+
+*/
